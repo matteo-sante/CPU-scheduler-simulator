6622ca5a128ecc6ca3fdf604b217376d3cf571d6
ranieri gabriele <ranigabri07@gmail.com>
2026-01-21 10:48:41 +0100

    secondo commit

---
+++ script.js
@@ -0,0 +1,48 @@
+console.log('Happy developing ✨')
+
+// DICHIARAZIONE VARIABILI GLOBALI
+let p = ["P1", "P2", "P3", "P4"]; //ARRAY PROCESSI
+let at = [4, 1, 3, 2]; //ARRAY TEMPO DI ARRIVO
+let bt = [2, 3, 4, 5]; //ARRAY TEMPO DI BURST
+let rbt = [2, 3, 4, 5]; //ARRAY TEMPO DI BURST RIMANENTE
+let pr = ["not needed", "not needed", "not needed", "not needed"]; //ARRAY PRIORITÀ
+
+/* FUNZIONE RESET
+ La tabella dei processi viene sostituita con una tabella vuota
+ Il contenuto dei div relativi all'output viene cancellato
+*/
+function reset(){
+   let tableEl = document.getElementById("idTable");
+   let oldTBodyEl = tableEl.getElementsByTagName('tbody')[0];
+   let newTBodyEl = document.createElement('tbody');
+
+   tableEl.replaceChild(newTBodyEl, oldTBodyEl);
+   document.getElementById("output").style.display = "none";
+}
+/* FUNZIONE START
+ si inseriscono nel corpo della tabella i dati dei processi  (nome, tempo di arrivo, tempo di burst, tempo di burst rimanente, priorità)  si mostra il diagramma di Gantt di attivazione dei processi
+*/
+function start(){
+   let i;
+   rbt = bt;
+   // si inseriscono nel corpo della tabella i dati dei processi 
+   let tableEl = document.getElementById("idTable");
+   let oldTBodyEl = tableEl.getElementsByTagName('tbody')[0];
+   let newTBodyEl = document.createElement("tbody");
+   for(i=0; i<p.length; i++) {
+       const trEl = newTBodyEl.insertRow();
+       let tdEl = trEl.insertCell();
+       tdEl.appendChild(document.createTextNode(p[i]));
+       tdEl = trEl.insertCell();
+       tdEl.appendChild(document.createTextNode(at[i]));
+       tdEl = trEl.insertCell();
+       tdEl.appendChild(document.createTextNode(bt[i]));
+       tdEl = trEl.insertCell();
+       tdEl.id = "idP" + i;
+       tdEl.appendChild(document.createTextNode(rbt[i]));
+       tdEl = trEl.insertCell();
+       tdEl.appendChild(document.createTextNode(pr[i]));
+   }
+   tableEl.replaceChild(newTBodyEl, oldTBodyEl);
+   document.getElementById("output").style.display = "block";
+}
